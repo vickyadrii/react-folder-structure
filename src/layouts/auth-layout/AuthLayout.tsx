f7d732a11ec0141507c5d558215ad271e6820b58
vickyadrii <vickyadri103@gmail.com>
2026-01-03 14:31:25 +0700

    chore: adjust custom color and auth layout

--- src/layouts/auth-layout/AuthLayout.tsx
+++ src/layouts/auth-layout/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 const AuthLayout = () => {
     return (
-        <div className="bg-red-500 text-white">
+        <div className="mx-auto flex h-screen max-h-screen max-w-xl items-center justify-center p-5">
             <Outlet />
         </div>
     );
